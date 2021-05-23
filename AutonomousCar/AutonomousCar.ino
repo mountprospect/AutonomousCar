f7a1b273b129f0386b23ea2bb77a1bdd49703ca8
mountprospect <maalav.pandya@gmail.com>
2021-05-22 23:01:14 -0400

    Added comments and title block. Final commit for 5-22-21

--- AutonomousCar/AutonomousCar.ino
+++ AutonomousCar/AutonomousCar.ino
@@ -1,15 +1,20 @@
+/**
+ * Autonomous Car
+ * Created by Maalv on 5/22/2021 @ 4:15PM
+ * Uses Arduino Nano and Ultrasonic Sensors to guide RC car Autonomously
+ */
+
 
 int trigPin = 2;
 int echoPin = 3;
-
-
-
-
 
 void setup() {
 
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+
+  Serial.begin(9600);
+  Serial.println("Program Running...\n");
 
 
 
@@ -29,6 +34,6 @@ float distance() {
    digitalWrite(trigPin, HIGH);
    delayMicroseconds(10);
    digitalWrite(trigPin, LOW);
-   duration = pulseIn(echoPin, HIGH);
+   duration = pulseIn(echoPin, HIGH); //Reads length of HIGH signal recieved by echoPin in microseconds
    return duration * 0.034 / 2;
 }
