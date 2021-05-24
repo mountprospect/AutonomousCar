a4e4d4ae52e1f7a75ab312f621d3febb9e8ad8df
mountprospect <maalav.pandya@gmail.com>
2021-05-24 10:43:06 -0400

    Added drive functionality

--- AutonomousCar/AutonomousCar.ino
+++ AutonomousCar/AutonomousCar.ino
@@ -1,17 +1,29 @@
 /**
- * Autonomous Car
- * Created by Maalv on 5/22/2021 @ 4:15PM
- * Uses Arduino Nano and Ultrasonic Sensors to guide RC car Autonomously
- */
+   Autonomous Car
+   Created by Maalv on 5/22/2021 @ 4:15PM
+   Uses Arduino Nano and Ultrasonic Sensors to guide RC car Autonomously
+*/
 
 
-int trigPin = 2;
-int echoPin = 3;
+int trigPinFront = 2;
+int echoPinFront = 3;
+
+int speedDrive = 6; //This pin controls the speed of the drive motor (ENB pin on IC)
+int driveDirOne = 4; //This pin controls the H-Bridge to change motor direction on the drive motor (IN3 on IC)
+int driveDirTwo = 5; //This pin controls the H-Bridge to change motor direction on the drive motor (IN4 on IC)
 
 void setup() {
 
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+  pinMode(trigPinFront, OUTPUT);
+  pinMode(echoPinFront, INPUT);
+
+  pinMode(speedDrive, OUTPUT);
+  pinMode(driveDirOne, OUTPUT);
+  pinMode(driveDirTwo, OUTPUT);
+
+  digitalWrite(speedDrive, LOW);
+  digitalWrite(driveDirOne, LOW);
+  digitalWrite(driveDirTwo, LOW);
 
   Serial.begin(9600);
   Serial.println("Program Running...\n");
@@ -22,18 +34,36 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  while(getDistance() >= 10) {
+    drive(255);
+    Serial.println(getDistance());
+  }
+  brake();
 
 }
 
 
-float distance() {
-   long duration;
-   digitalWrite(trigPin, LOW);
-   delayMicroseconds(2);
-   digitalWrite(trigPin, HIGH);
-   delayMicroseconds(10);
-   digitalWrite(trigPin, LOW);
-   duration = pulseIn(echoPin, HIGH); //Reads length of HIGH signal recieved by echoPin in microseconds
-   return duration * 0.034 / 2;
+long getDistance() {
+  long duration, distance;
+  digitalWrite(trigPinFront, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPinFront, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPinFront, LOW);
+  duration = pulseIn(echoPinFront, HIGH); //Reads length of HIGH signal recieved by echoPinFront in microseconds
+  distance = ((duration * 0.034) / 2) / 2.54; //Dividing by final 2.54 to convert from cm -> in
+  Serial.println(distance);
+  return distance;
+}
+
+void drive(int speed) {
+  analogWrite(speedDrive, speed);
+  digitalWrite(driveDirOne, LOW);
+  digitalWrite(driveDirTwo, HIGH);
+}
+
+void brake() {
+  digitalWrite(driveDirOne, LOW);
+  digitalWrite(driveDirTwo, LOW);
+
 }
